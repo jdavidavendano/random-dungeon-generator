0d74a72dee76157d5954aef829e33c1e5cf8b2f8
Lusuarezb <lusuarezb@unal.edu.co>
2022-02-02 22:46:46 -0500

    Cambios varios

--- DungeonGenerator/Assets/Scripts/Player/PlayerMovement.cs
+++ DungeonGenerator/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-enum PlayerState {
+enum PlayerState
+{
     walk,
     attack
 }
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
     [SerializeField] private float _moveSpeed = 12f;
     private PlayerState _currentState;
@@ -15,60 +17,80 @@ public class PlayerMovement : MonoBehaviour {
     private Animator _animator;
     private Vector3 _movement;
 
-    void Start() {
+    private bool _isAttacking;
+
+    void Start()
+    {
         _currentState = PlayerState.walk;
         _rigidBody = GetComponent<Rigidbody2D>();
         _animator = GetComponentInChildren<Animator>();
+
+        _isAttacking = false;
     }
 
-    void Update() {
+    void Update()
+    {
+        // Input del movimiento
+        _movement = Vector3.zero;
+        _movement.x = Input.GetAxisRaw("Horizontal"); // * _moveSpeed * Time.fixedDeltaTime;
+        _movement.y = Input.GetAxisRaw("Vertical");
+        _movement.Normalize();
+
+
         // Input
-        if (Input.GetMouseButtonDown(0) && _currentState != PlayerState.attack) {
+        if (Input.GetMouseButtonDown(0) && _currentState != PlayerState.attack)
+        {
+            _isAttacking = true;
             Debug.Log("click");
             StartCoroutine(AttackCo());
         }
-        else if (_currentState == PlayerState.walk) {
-            UpdateAnimationAndMove();
-        }
+
     }
 
     // Como el framerate puede cambiar, se manejan las físicas aquí
     // Ejecutado en un tiempo no encapsulado por el framerate (50 veces por segundo)
-    void FixedUpdate() {
-        // Input del movimiento
-        _movement = Vector3.zero;
-        _movement.x = Input.GetAxisRaw("Horizontal") * _moveSpeed * Time.fixedDeltaTime;
-        _movement.y = Input.GetAxisRaw("Vertical") * _moveSpeed * Time.fixedDeltaTime;
-        _movement.Normalize();
+    void FixedUpdate()
+    {
+        if (_currentState == PlayerState.walk && _isAttacking == false)
+        {
+            UpdateAnimationAndMove();
+        }
     }
 
-    void UpdateAnimationAndMove() {
+    void UpdateAnimationAndMove()
+    {
         // Movimiento
-        if (_movement != Vector3.zero) {
+        if (_movement != Vector3.zero)
+        {
             MoveCharacter();
 
             _animator.SetFloat("Horizontal", _movement.x);
             _animator.SetFloat("Vertical", _movement.y);
             _animator.SetBool("Moving", true);
         }
-        else {
+        else
+        {
             _animator.SetBool("Moving", false);
         }
     }
 
-    void MoveCharacter() {
+    void MoveCharacter()
+    {
         _rigidBody.MovePosition(transform.position + _movement * _moveSpeed * Time.deltaTime);
     }
 
-    private IEnumerator AttackCo() {
+    private IEnumerator AttackCo()
+    {
         _animator.SetBool("Attacking", true);
         _currentState = PlayerState.attack;
 
         yield return null; // Introduce un pequeño delay en la acción
-        
+
         _animator.SetBool("Attacking", false);
 
         yield return new WaitForSeconds(0.3f); // Esperar mientras termina la animación
         _currentState = PlayerState.walk;
+
+        _isAttacking = false;
     }
 }
